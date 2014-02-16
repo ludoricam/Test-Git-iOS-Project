53835a894d323c0cc694c88ecd5adfd7092fb675
lulu <lulu@lulu.local>
2014-02-16 16:56:14 +1300

    added a test class

---
+++ Git Test/LAHTest.h
@@ -0,0 +1,13 @@
+//
+//  LAHTest.h
+//  Git Test
+//
+//  Created by lulu on 16/02/14.
+//  Copyright (c) 2014 Louise Hathaway. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LAHTest : NSObject
+
+@end
---
+++ Git Test/LAHTest.m
@@ -0,0 +1,13 @@
+//
+//  LAHTest.m
+//  Git Test
+//
+//  Created by lulu on 16/02/14.
+//  Copyright (c) 2014 Louise Hathaway. All rights reserved.
+//
+
+#import "LAHTest.h"
+
+@implementation LAHTest
+
+@end
